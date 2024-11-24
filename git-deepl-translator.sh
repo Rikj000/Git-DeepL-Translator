7be66cbf6828ed0a9ce8d5d1bfb6f2c864be5f72
Rikj000 <rik_helsen@hotmail.com>
2024-11-24 09:10:29 +0100

    ⚡️ Updated `git-deepl-translator` - Switched to `deeplx` from `deepl-cli` - Added input argument support - Added `update` function

--- git-deepl-translator.sh
+++ git-deepl-translator.sh
@@ -1,26 +1,111 @@
 #!/bin/bash
 # Author: Rikj000
 
-# Dependencies
-# - git: https://archlinux.org/packages/extra/x86_64/git/
-# - git-filter-repo: https://archlinux.org/packages/extra/any/git-filter-repo/
-# - deepl-cli: https://kojix2.github.io/deepl-cli/
-
-# ToDo - Switch to input arguments
-# == SETTINGS ==========================================================================================================
-DEEPL_AUTH_KEY="";
-DEEPL_FREE_API=true;
-DEEPL_FREE_LINES_PER_BATCH=1000;
-GIT_REPO_INPUT_DIR="";
-GIT_REPO_OUTPUT_DIR="";
+# === Default Settings =================================================================================================
+DEEPLX_URL="http://localhost:1188";
+DEEPLX_RETRY_SECONDS="10";
+INPUT_GIT_REPO_DIR="";
+OUTPUT_GIT_REPO_DIR="";
 INPUT_LANGUAGE="JA";
 OUTPUT_LANGUAGE="EN";
-# == SETTINGS ==========================================================================================================
+# === Default Settings =================================================================================================
+
+# Help function
+usage() {
+    cat << EOF
+
+  Git-DeepL-Translator - v0.0.0
+  Simple "bash" script to translate the whole commit message history,
+  of a local Git repository, from one language to another, using DeepLX!
+
+  Usage:
+    git-deepl-translator [options]
+
+  Example:
+    git-deepl-translator -ig="/path/to/git/input/repo" -og="/path/to/git/output/repo" -il 'FR' -ol 'EN'
+
+  Optional options:
+    -h,    -help                           Show this help.
+    -u,    -update                         Update Git-DeepL-Translator to the latest version.
+    -dl,   -deeplx_url=<url>               URL to the DeepLX API, defaults to 'http://localhost:1188'
+    -ds,   -deeplx_retry_seconds=<sec>     Amount of seconds to wait before retrying to fetch a DeepLX translation in case none was received, defaults to 10
+    -ig,   -input_git_repo_dir=<path>      Path to local input Git repository
+    -og,   -output_git_repo_dir=<path>     Path to local output Git repository
+    -il,   -input_language=<lang>          Language used by the commit messages of the input Git repository, defaults to 'JA' (Japanese)
+    -ol,   -output_language=<lang>         Language to translate the commit messages of the output Git repository into, defaults to 'EN' (English)
+EOF
+
+    exit 0;
+}
+
+# Update function
+update() {
+    echo "Setting up installation directory helpers...";
+    INSTALLATION_FILE=$(realpath "$(dirname "$0")/$(basename "$0")");
+    INSTALLATION_DIR=$(dirname "$INSTALLATION_FILE");
+
+    echo "Moving to the installation directory... ($INSTALLATION_DIR)";
+    cd "$INSTALLATION_DIR/" || exit;
+
+    echo "Downloading + installing latest version...";
+    wget "$(
+        curl -s -H "Accept: application/vnd.github.v3+json" \
+        'https://api.github.com/repos/Rikj000/Git-DeepL-Translator/releases/latest' \
+        | jq .assets[0].browser_download_url)";
+    exit;
+}
 
 # ANSI text coloring
 GREEN='\033[0;32m';
 RED='\033[0;31m';
 END='\033[m';
+
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+        -du=*|-deeplx_url=*)
+        DEEPLX_URL="${arg#*=}";
+        shift
+        ;;
+        -ds=*|-deeplx_retry_seconds=*)
+        DEEPLX_RETRY_SECONDS="${arg#*=}";
+        shift
+        ;;
+        -ig=*|-input_git_repo_dir=*)
+        INPUT_GIT_REPO_DIR="${arg#*=}";
+        shift
+        ;;
+        -og=*|-output_git_repo_dir=*)
+        OUTPUT_GIT_REPO_DIR="${arg#*=}";
+        shift
+        ;;
+        -il=*|-input_language=*)
+        INPUT_LANGUAGE="${arg#*=}";
+        shift
+        ;;
+        -ol=*|-output_language=*)
+        OUTPUT_LANGUAGE="${arg#*=}";
+        shift
+        ;;
+        -h|-help)
+        usage;
+        shift
+        ;;
+        -u|-update)
+        update;
+        shift
+        ;;
+        *)
+        echo "";
+        echo -e "${RED}  🙉  git-deepl-translator - Illegal argument(s) used!${END}";
+        echo "";
+        echo "  Please see the 'git-deepl-translator -help' output below for the correct usage:";
+        usage;
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
 
 echo "Setting up directory helpers...";
 INSTALLATION_FILE=$(realpath "$(dirname "$0")/$(basename "$0")");
@@ -30,7 +115,6 @@ TMP_DIR="$INSTALLATION_DIR/tmp";
 TMP_GIT_REPO_DIR="$TMP_DIR/git";
 TMP_GIT_LOG_DIR="$TMP_DIR/git-log";
 TMP_GIT_LOG_FILE="$TMP_GIT_LOG_DIR/git-log.txt";
-TMP_GIT_LOG_TRANSLATED_FILE="$TMP_GIT_LOG_DIR/git-log-translated.txt";
 TMP_GIT_LOG_EXPRESSIONS_FILE="$TMP_GIT_LOG_DIR/git-log-expressions.txt";
 
 echo "Cleaning up temporary conversion directory ($TMP_DIR)...";
@@ -38,88 +122,71 @@ echo "Cleaning up temporary conversion directory ($TMP_DIR)...";
 yes | rm -fr "$TMP_DIR";
 mkdir "$TMP_DIR" "$TMP_GIT_LOG_DIR";
 
-echo "Copying the Git repository input directory ($GIT_REPO_INPUT_DIR)" \
+echo "Copying the Git repository input directory ($INPUT_GIT_REPO_DIR)" \
     "to the temporary Git conversion directory ($TMP_GIT_REPO_DIR)...";
-cp -r "$GIT_REPO_INPUT_DIR" "$TMP_GIT_REPO_DIR";
+cp -r "$INPUT_GIT_REPO_DIR" "$TMP_GIT_REPO_DIR";
 
 echo "Moving to temporary Git conversion directory ($TMP_GIT_REPO_DIR)...";
-cd "$GIT_REPO_INPUT_DIR/" || exit;
+cd "$TMP_GIT_REPO_DIR/" || exit;
 
 echo "Log git commit message history to temporary git-log file ($TMP_GIT_LOG_FILE), include new-lines..."
 git log --pretty=format:"%B" > "$TMP_GIT_LOG_FILE";
 
-echo "Moving to the installation directory... ($INSTALLATION_DIR)";
-cd "$INSTALLATION_DIR/" || exit;
-
-TMP_GIT_LOG_FILE_LINES=$(wc -l < "$TMP_GIT_LOG_FILE");
-if [ $DEEPL_FREE_API ]; then
-    touch "$TMP_GIT_LOG_TRANSLATED_FILE";
-    DEEPL_BATCHES_FLOAT=$(bc -l <<< "$TMP_GIT_LOG_FILE_LINES/$DEEPL_FREE_LINES_PER_BATCH");
-    DEEPL_BATCHES_INT=$(echo "($DEEPL_BATCHES_FLOAT+0.5)/1" | bc);
-
-    echo "Splitting total of '$TMP_GIT_LOG_FILE_LINES' lines into '$DEEPL_BATCHES_INT' batches" \
-        "of max '$DEEPL_FREE_LINES_PER_BATCH' lines per batch...";
-    for BATCH_INDEX in $(seq "$DEEPL_BATCHES_INT"); do
-        DEEPL_BATCH_HEAD_INT=$(bc <<< "$BATCH_INDEX*$DEEPL_FREE_LINES_PER_BATCH");
-        TMP_GIT_LOG_BATCH_FILE="$TMP_GIT_LOG_DIR/git-log-$BATCH_INDEX.txt";
-        TMP_GIT_LOG_TRANSLATED_BATCH_FILE="$TMP_GIT_LOG_DIR/git-log-translated-$BATCH_INDEX.txt";
-
-        if [ "$BATCH_INDEX" -ne "$DEEPL_BATCHES_INT" ]; then
-            echo "Converting '$DEEPL_FREE_LINES_PER_BATCH' lines in batch '$BATCH_INDEX/$DEEPL_BATCHES_INT'...";
-            head -n "$DEEPL_BATCH_HEAD_INT" "$TMP_GIT_LOG_FILE" | \
-            tail -n "$DEEPL_FREE_LINES_PER_BATCH" > "$TMP_GIT_LOG_BATCH_FILE";
-        else
-            # Handle last iteration
-            DEEPL_BATCH_TAIL_INT=$(
-                bc <<< "$TMP_GIT_LOG_FILE_LINES-(($DEEPL_BATCHES_INT-1)*$DEEPL_FREE_LINES_PER_BATCH)");
-            echo "Converting '$DEEPL_BATCH_TAIL_INT' lines in batch '$BATCH_INDEX/$DEEPL_BATCHES_INT'...";
-            tail -n "$DEEPL_BATCH_TAIL_INT" "$TMP_GIT_LOG_FILE" > "$TMP_GIT_LOG_BATCH_FILE";
-        fi
-
-        echo "Converting batch '$BATCH_INDEX/$DEEPL_BATCHES_INT'" \
-            "to temporary git-log translated batch file ($TMP_GIT_LOG_BATCH_FILE)...";
-        DEEPL_AUTH_KEY="$DEEPL_AUTH_KEY" deepl \
-            "$TMP_GIT_LOG_BATCH_FILE" \
-            --output "$TMP_GIT_LOG_TRANSLATED_BATCH_FILE" \
-            --from "$INPUT_LANGUAGE" \
-            --to "$OUTPUT_LANGUAGE";
-
-        echo "Appending temporary git-log translated batch file ($TMP_GIT_LOG_BATCH_FILE)" \
-            "to temporary git-log translated file ($TMP_GIT_LOG_TRANSLATED_FILE)..."
-        cat "$TMP_GIT_LOG_TRANSLATED_BATCH_FILE" >> "$TMP_GIT_LOG_TRANSLATED_FILE";
-    done
-else
-    echo "Converting all '$TMP_GIT_LOG_FILE_LINES' lines in one batch" \
-        "to temporary git-log translated file ($TMP_GIT_LOG_TRANSLATED_FILE)...";
-    DEEPL_AUTH_KEY="$DEEPL_AUTH_KEY" deepl \
-        "$TMP_GIT_LOG_FILE" \
-        --output "$TMP_GIT_LOG_TRANSLATED_FILE" \
-        --from "$INPUT_LANGUAGE" \
-        --to "$OUTPUT_LANGUAGE";
-fi
-
-echo "Combining temporary git-log file ($TMP_GIT_LOG_FILE)" \
-    "with temporary git-log translated file ($TMP_GIT_LOG_TRANSLATED_FILE)" \
+echo "Combining temporary git-log file ($TMP_GIT_LOG_FILE) lines with DeepLX translations" \
     "into temporary git-log expressions file ($TMP_GIT_LOG_EXPRESSIONS_FILE)...";
 touch "$TMP_GIT_LOG_EXPRESSIONS_FILE";
+TMP_GIT_LOG_FILE_LINES=$(wc -l < "$TMP_GIT_LOG_FILE");
 for LINE_INDEX in $(seq "$TMP_GIT_LOG_FILE_LINES"); do
-    ORIGINAL_LINE=$(head -n "$LINE_INDEX" "$TMP_GIT_LOG_FILE" | tail -n 1);
-    TRANSLATED_LINE=$(head -n "$LINE_INDEX" "$TMP_GIT_LOG_TRANSLATED_FILE" | tail -n 1);
-    echo "$ORIGINAL_LINE==>$TRANSLATED_LINE" >> "$TMP_GIT_LOG_EXPRESSIONS_FILE";
-done
 
-echo "Moving to temporary Git conversion directory ($TMP_GIT_REPO_DIR)...";
-cd "$GIT_REPO_INPUT_DIR/" || exit;
+    ORIGINAL_LINE=$(head -n "$LINE_INDEX" "$TMP_GIT_LOG_FILE" | tail -n 1);
+    # Replace 'TAB' with 'space' to prevent DeepLX from crashing
+    FORMATTED_LINE=${ORIGINAL_LINE//	/ };
+    FORMATTED_LINE=${FORMATTED_LINE//　/ };
+    # Replace 'double quote' with 'single quote' to prevent DeepLX from crashing
+    FORMATTED_LINE=${FORMATTED_LINE//\"/\'}
+    # Replace '/' with '' to prevent DeepLX from crashing
+    FORMATTED_LINE=${FORMATTED_LINE//\\}
+
+    # Replace 'space' with '' to check if line empty
+    if [ -z "${FORMATTED_LINE// }" ] &>/dev/null; then
+        echo "Line: $LINE_INDEX - Skipping empty line...";
+        continue;
+    fi
+
+    TRANSLATED_LINE="null";
+    while [ "$TRANSLATED_LINE" == "null" ]; do
+        TRANSLATED_LINE=$(curl -s -X POST "$DEEPLX_URL/translate" \
+        -H "Content-Type: application/json" \
+        -d "{
+            \"text\": \"$FORMATTED_LINE\",
+            \"source_lang\": \"$INPUT_LANGUAGE\",
+            \"target_lang\": \"$OUTPUT_LANGUAGE\"
+        }" | jq -r '.data');
+
+        if [ "$TRANSLATED_LINE" == "null" ]; then
+            echo "Line: $LINE_INDEX - Received null translation for line '$FORMATTED_LINE'," \
+                "re-trying in '$DEEPLX_RETRY_SECONDS' seconds...";
+            echo "Likely rate-limited, either wait it out, or switch VPN connections + restart 'deeplx' to continue...";
+            sleep "$DEEPLX_RETRY_SECONDS";
+        fi
+    done
+
+    # Only append if $TRANSLATED_LINE not empty
+    if [ -n "$TRANSLATED_LINE" ]; then
+        echo "Line: $LINE_INDEX - Appending translated line!";
+        echo "$ORIGINAL_LINE==>$TRANSLATED_LINE" >> "$TMP_GIT_LOG_EXPRESSIONS_FILE";
+    fi
+done
 
 echo "Replace git commit message history with temporary git-log expressions file ($TMP_GIT_LOG_EXPRESSIONS_FILE)...";
 git filter-repo --force --replace-message "$TMP_GIT_LOG_EXPRESSIONS_FILE";
 
-echo "Cleaning up Git repository output directory ($GIT_REPO_OUTPUT_DIR)...";
+echo "Cleaning up Git repository output directory ($OUTPUT_GIT_REPO_DIR)...";
 # shellcheck disable=SC2216
-yes | rm -fr "$GIT_REPO_OUTPUT_DIR";
+yes | rm -fr "$OUTPUT_GIT_REPO_DIR";
 
 echo "Moving the temporary Git conversion directory ($TMP_GIT_REPO_DIR)" \
-    "to Git repository output directory ($GIT_REPO_OUTPUT_DIR)...";
-mv "$TMP_GIT_REPO_DIR" "$GIT_REPO_OUTPUT_DIR";
+    "to Git repository output directory ($OUTPUT_GIT_REPO_DIR)...";
+mv "$TMP_GIT_REPO_DIR" "$OUTPUT_GIT_REPO_DIR";
 
 echo -e "${GREEN}git-deepl-translator has been executed to its end!${END}";
