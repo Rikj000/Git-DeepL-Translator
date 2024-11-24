2c582ebedab6be508e7f932bbba3501c2079c376
Rikj000 <rik_helsen@hotmail.com>
2024-11-24 13:58:58 +0100

    ⚡️ Updated `git-deepl-translator` - Strip duplicate lines from `$TMP_GIT_LOG_FILE`

--- git-deepl-translator.sh
+++ git-deepl-translator.sh
@@ -178,6 +178,9 @@ git log --all --pretty=format:"%B" > "$TMP_GIT_LOG_FILE";
 echo "Strip empty new lines from temporary git-log file ($TMP_GIT_LOG_FILE)..."
 sed -i '/^$/d' "$TMP_GIT_LOG_FILE";
 
+echo "Strip duplicate lines from temporary git-log file ($TMP_GIT_LOG_FILE)..."
+awk -i inplace '!seen[$0]++' "$TMP_GIT_LOG_FILE";
+
 echo "Combining temporary git-log file ($TMP_GIT_LOG_FILE) lines with DeepLX translations" \
     "into temporary git-log expressions file ($TMP_GIT_LOG_EXPRESSIONS_FILE)...";
 touch "$TMP_GIT_LOG_EXPRESSIONS_FILE";
